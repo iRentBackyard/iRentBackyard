46bb4913ba740b5b798b65fee1f6cfa896d476c3
akshaysn1991 <akshaysn1991@gmail.com>
2016-09-21 16:50:29 +0000

    first commit

---
+++ app/models/app_constants.rb
@@ -0,0 +1,13 @@
+module AppConstants
+    FACEBOOK_PAGE_URL = "#"
+    TWITTER_URL = "#"
+    TUMBLR_URL = "#"
+    LINKEDIN_URL = "#"
+    GOOGLE_PLUS_URL = "#"
+    TRABOL_BLOG_URL = "#"
+    FEED_BURNER_URL = "#"
+    YOUTUBE_URL = "#"
+    PINTEREST_URL = "#"
+    GOOGLE_PLUS_API_KEY = "#"
+    GOOGLE_PLUS_PAGE_ID = "#"
+end
